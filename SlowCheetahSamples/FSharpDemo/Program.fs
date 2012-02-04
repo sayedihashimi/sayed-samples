eb420c8a00ed04cff859577f54ad8c1b9928ec59
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2012-02-04 12:07:47 -0800

    Updates to SlowCheetah samples

--- SlowCheetahSamples/FSharpDemo/Program.fs
+++ SlowCheetahSamples/FSharpDemo/Program.fs
@@ -1,4 +1,4 @@
-﻿// Learn more about F# at http://fsharp.net
+﻿// A simple application to create the config and show results in the console
 let settingOne = System.Configuration.ConfigurationManager.AppSettings.["settingOne"];
 let settingTwo = System.Configuration.ConfigurationManager.AppSettings.["settingTwo"];
 let settingthree = System.Configuration.ConfigurationManager.AppSettings.["settingThree"];
