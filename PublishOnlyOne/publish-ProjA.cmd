a96606044075d99dbce6ab913183e3b94101a81d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-03-05 18:56:23 -0800

    minor updates

--- PublishOnlyOne/publish-ProjA.cmd
+++ PublishOnlyOne/publish-ProjA.cmd
@@ -1,4 +1,4 @@
-echo This command will fail because ProjB does not have a publish profile named 'siteone - Web Deploy'
+
 echo This script assumes that the Web Deploy password is set at %password%
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe PublishOnlyOne.sln /p:Configuration=Release /p:VisualStudioVersion=11.0 /p:DeployOnBuild=true /p:PublishProfile="siteone - Web Deploy" /p:Password=%password%
 
