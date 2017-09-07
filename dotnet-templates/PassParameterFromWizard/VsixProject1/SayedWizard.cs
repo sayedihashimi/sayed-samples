813dbaf03d82c9c52f560666619d4f1cb648b9a5
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-09-06 20:49:42 -0700

    adding template sample passing a parameter from a wizard

---
+++ dotnet-templates/PassParameterFromWizard/VsixProject1/SayedWizard.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+
+namespace VsixProject1 {
+    public class SayedWizard : IWizard {
+        public void BeforeOpeningFile(ProjectItem projectItem) {
+        }
+
+        public void ProjectFinishedGenerating(Project project) {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem) {
+        }
+
+        public void RunFinished() {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams) {
+            replacementsDictionary.Add("passthrough:copyrightName", "value from wizard");
+        }
+
+        public bool ShouldAddProjectItem(string filePath) {
+            return true;
+        }
+    }
+}
