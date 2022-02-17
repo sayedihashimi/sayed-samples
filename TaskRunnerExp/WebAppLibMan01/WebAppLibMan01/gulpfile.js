3b1252fdd65de8dbf812b7562732d3f59c019bee
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-02-17 09:51:45 -0500

    adding sample

---
+++ TaskRunnerExp/WebAppLibMan01/WebAppLibMan01/gulpfile.js
@@ -0,0 +1,22 @@
+﻿const { series } = require('gulp');
+
+function defaultTask(cb) {
+    // place code for your default task here
+    console.log('task runner started: defaultTask');
+    cb();    
+}
+
+function beforeBuild(cb) {
+    console.log("before build called");
+    cb();
+}
+
+function afterBuild(cb) {
+    console.log("after build called");
+    cb();
+}
+
+// exports.default = series(defaultTask, beforeBuild, afterBuild);
+exports.default = defaultTask;
+exports.beforeBuild = beforeBuild;
+exports.afterBuild = afterBuild;
