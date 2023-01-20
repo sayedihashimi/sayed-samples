222f30c237e6f1a04af2b90c9bb18a36b53fa4e1
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2023-01-20 13:33:52 -0500

    minor changes

--- MyApiMinimal/Program.cs
+++ MyApiMinimal/Program.cs
@@ -1,24 +1,8 @@
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGet("/users/{userId}/books/{bookId}",
+    (int userId) => $"The user id is {userId} and book id is ");
 
 app.Run();
