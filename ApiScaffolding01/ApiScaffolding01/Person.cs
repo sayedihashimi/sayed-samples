d18169fc176ab9c426f2113fdf4344065c8f2f67
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2023-03-23 13:49:15 -0400

    adding a model class

---
+++ ApiScaffolding01/ApiScaffolding01/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiScaffolding01 {
+    public class Person {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+    }
+}
