ce215bc4ff8c2faaed0d0e81800f691b23f2c2ee
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-05-14 11:56:30 -0400

    getting openapi exploer ready

---
+++ SayedHa.OpenAPIExplorer/SayedHa.OpenAPIExplorer.ConsoleRunner/Explorer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi;
+using System.Diagnostics;
+
+namespace SayedHa.OpenAPIExplorer.ConsoleRunner {
+	public class Explorer {
+		public Explorer(string openApiSpecFilepath) {
+			Document = new OpenApiStreamReader().Read(File.OpenRead(openApiSpecFilepath), out var diagnostic);
+		}
+
+		internal OpenApiDocument? Document { get; private set; }
+	}
+}
