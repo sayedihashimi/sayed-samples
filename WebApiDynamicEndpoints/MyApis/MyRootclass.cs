e2b3ae571690f631c266e5f4f78c0be0e826d965
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-05-21 12:54:19 -0400

    adding webapi with dynamic endpoint discovery

---
+++ WebApiDynamicEndpoints/MyApis/MyRootclass.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace MyApis
+{
+    public class MyRootClass
+    {
+        public static void AddRoutes(WebApplication app)
+        {
+
+            var summaries = new[]
+            {
+                "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.MapGet("/coolstuff", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new CoolStuff
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetCoolStuff")
+            .WithOpenApi();
+        }
+    }
+
+    internal record CoolStuff(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
