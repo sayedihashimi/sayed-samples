57573982be29bb0464aa3b2621bcf4ea2f0178f6
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-05-01 00:07:17 -0400

    wip, fixing tests

---
+++ Sayedha.Analyzers/Sayedha.Analyzers.Tests2/Class1.cs
@@ -0,0 +1,8 @@
+﻿public static class Program2 {
+    public static void Main2() {
+        var handler = new HttpClientHandler {
+            ServerCertificateCustomValidationCallback = (_, __, ___, ____) => true
+        };
+    }
+}
+
