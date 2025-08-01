e567a0e0ce8740ce56841cd0c701d12fc3f374ea
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-08-01 13:35:57 -0400

    jsps sample

--- 2025.07.jsps.new.project/reactjsps/vite.config.js
+++ 2025.07.jsps.new.project/reactjsps/vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 50459,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        plugins: [react()],
+        server: {
+            // port: parseInt(process.env.PORT ?? "5173"),
+            proxy: {
+                '/api': {
+                    target:
+                        env.services__apiservice__https__0 ||
+                        env.services__apiservice__http__0,
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: (path) => path.replace(/^\/api/, "")
+                }
+            }
+        }
     }
 })
